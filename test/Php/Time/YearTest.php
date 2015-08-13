3becb72437cbf5444d894c1d4bfdaa9b9428b562
Steffen Hanikel <steffen.hanikel@meinfernbus.de>
2015-08-13 16:54:52 +0200

    Setup composer and phpunit

---
+++ test/Php/Time/YearTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Php\Time;
+
+use PHPUnit_Framework_TestCase;
+
+class YearTest extends PHPUnit_Framework_TestCase
+{
+    public function testAsd() {
+        Year::init();
+    }
+
+}
