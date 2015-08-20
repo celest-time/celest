4b048e07eb1c5df2fa2281e6aa7ba2697e3a478d
Steffen Hanikel <steffen.hanikel@meinfernbus.de>
2015-08-20 15:06:36 +0200

    Added some stubs

---
+++ src/Php/Time/Format/DateTimeFormatterBuilder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Php\Time\Format;
+
+
+class DateTimeFormatterBuilder
+{
+
+    /**
+     * @param $YEAR
+     * @param $int
+     * @param $int1
+     * @param $EXCEEDS_PAD
+     * @return $this
+     */
+    public function appendValue($YEAR, $int, $int1, $EXCEEDS_PAD)
+    {
+        return $this;
+    }
+
+    public function toFormatter()
+    {
+    }
+}
---
+++ src/Php/Time/Format/SignStyle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hanikel
+ * Date: 20.08.15
+ * Time: 14:57
+ */
+
+namespace Php\Time\Format;
+
+
+class SignStyle
+{
+
+    const EXCEEDS_PAD = null;
+}
