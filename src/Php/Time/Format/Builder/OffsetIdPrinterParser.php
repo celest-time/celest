f21f13a65150238797a3e8ddea15e654f88324d0
Steffen Hanikel <steffen.hanikel@meinfernbus.de>
2016-01-03 19:31:52 +0100

    Added ZoneOffsetIdPrinter Test

--- src/Php/Time/Format/Builder/OffsetIdPrinterParser.php
+++ src/Php/Time/Format/Builder/OffsetIdPrinterParser.php
@@ -8,159 +8,171 @@
 
 namespace Php\Time\Format\Builder;
 
+use Php\Time\Format\DateTimeParseContext;
+use Php\Time\Format\DateTimePrintContext;
+use Php\Time\Helper\Math;
+use Php\Time\Temporal\ChronoField;
+use Symfony\Component\Yaml\Exception\RuntimeException;
+
 
 /**
  * Prints or parses an offset ID.
  */
-static final class OffsetIdPrinterParser implements DateTimePrinterParser
+final class OffsetIdPrinterParser implements DateTimePrinterParser
 {
-static final String[] PATTERNS = new String[]
-{
-"+HH", "+HHmm", "+HH:mm", "+HHMM", "+HH:MM", "+HHMMss", "+HH:MM:ss", "+HHMMSS", "+HH:MM:SS",
-};  // order used in pattern builder
-        static final OffsetIdPrinterParser INSTANCE_ID_Z = new OffsetIdPrinterParser("+HH:MM:ss", "Z");
-        static final OffsetIdPrinterParser INSTANCE_ID_ZERO = new OffsetIdPrinterParser("+HH:MM:ss", "0");
+    public static $PATTERNS =
+        [
+            "+HH", "+HHmm", "+HH:mm", "+HHMM", "+HH:MM", "+HHMMss", "+HH:MM:ss", "+HHMMSS", "+HH:MM:SS",
+        ];  // order used in pattern builder
 
-        private final String noOffsetText;
-        private final int type;
+//static final OffsetIdPrinterParser INSTANCE_ID_Z = new OffsetIdPrinterParser("+HH:MM:ss", "Z");
+//static final OffsetIdPrinterParser INSTANCE_ID_ZERO = new OffsetIdPrinterParser("+HH:MM:ss", "0");
 
-        /**
-         * Constructor.
-         *
-         * @param $pattern  the pattern
-         * @param $noOffsetText  the text to use for UTC, not null
-         */
-        OffsetIdPrinterParser(String pattern, String noOffsetText) {
-    Objects->requireNonNull(pattern, "pattern");
-    Objects->requireNonNull(noOffsetText, "noOffsetText");
-    this->type = checkPattern(pattern);
-    this->noOffsetText = noOffsetText;
-}
+    /** @var string */
+    private $noOffsetText;
+    /** @var int */
+    private $type;
 
-        private int checkPattern(String pattern) {
-    for (int i = 0; i < PATTERNS->length;
-    i++) {
-        if (PATTERNS[i]->equals(pattern)) {
-            return i;
-        }
-            }
-            throw new IllegalArgumentException("Invalid zone offset pattern: " + pattern);
-        }
-
-        @Override
-        public boolean format(DateTimePrintContext context, StringBuilder buf) {
-    Long offsetSecs = context->getValue(OFFSET_SECONDS);
-            if (offsetSecs == null) {
-                return false;
-            }
-            int totalSecs = Math->toIntExact(offsetSecs);
-            if (totalSecs == 0) {
-                buf->append(noOffsetText);
-            } else {
-                int absHours = Math->abs((totalSecs / 3600) % 100);  // anything larger than 99 silently dropped
-                int absMinutes = Math->abs((totalSecs / 60) % 60);
-                int absSeconds = Math->abs(totalSecs % 60);
-                int bufPos = buf->length();
-                int output = absHours;
-                buf->append(totalSecs < 0 ? "-" : "+")
-                    ->append((char) (absHours / 10 + '0')).append((char) (absHours % 10 + '0'));
-                if (type >= 3 || (type >= 1 && absMinutes > 0)) {
-                    buf->append((type % 2) == 0 ? ":" : "")
-                        ->append((char) (absMinutes / 10 + '0')).append((char) (absMinutes % 10 + '0'));
-                    output += absMinutes;
-                    if (type >= 7 || (type >= 5 && absSeconds > 0)) {
-                        buf->append((type % 2) == 0 ? ":" : "")
-                            ->append((char) (absSeconds / 10 + '0')).append((char) (absSeconds % 10 + '0'));
-                        output += absSeconds;
-                    }
-                }
-                if (output == 0) {
-                    buf->setLength(bufPos);
-                    buf->append(noOffsetText);
-                }
-            }
-            return true;
-        }
-
-        @Override
-        public int parse(DateTimeParseContext context, CharSequence text, int position) {
-    int length = text->length();
-            int noOffsetLen = noOffsetText->length();
-            if (noOffsetLen == 0) {
-                if (position == length) {
-                    return context->setParsedField(OFFSET_SECONDS, 0, position, position);
-                }
-            } else {
-                if (position == length) {
-                    return ~position;
-                }
-                if (context->subSequenceEquals(text, position, noOffsetText, 0, noOffsetLen)) {
-                    return context->setParsedField(OFFSET_SECONDS, 0, position, position + noOffsetLen);
-                }
-            }
-
-            // parse normal plus/minus offset
-            char sign = text->charAt(position);  // IOOBE if invalid position
-            if (sign == '+' || sign == '-') {
-                // starts
-                int negative = (sign == '-' ? -1 : 1);
-                int[] array = new int[4];
-                array[0] = position + 1;
-                if ((parseNumber(array, 1, text, true) ||
-                    parseNumber(array, 2, text, type >= 3) ||
-                        parseNumber(array, 3, text, false)) == false){
-                        // success
-                    long offsetSecs = negative * (array[1] * 3600L + array[2] * 60L + array[3]);
-                    return context->setParsedField(OFFSET_SECONDS, offsetSecs, position, array[0]);
-                }
-            }
-            // handle special case of empty no offset text
-            if (noOffsetLen == 0) {
-                return context->setParsedField(OFFSET_SECONDS, 0, position, position + noOffsetLen);
-            }
-            return ~position;
-        }
-
-        /**
-         * Parse a two digit zero-prefixed number.
-         *
-         * @param $array  the array of parsed data, 0=pos,1=hours,2=mins,3=secs, not null
-         * @param $arrayIndex  the index to parse the value into
-         * @param $parseText  the offset ID, not null
-         * @param $required  whether this number is required
-         * @return true if an error occurred
-         */
-        private boolean parseNumber(int[] array, int arrayIndex, CharSequence parseText, boolean required) {
-    if ((type + 3) / 2 < arrayIndex) {
-        return false;  // ignore seconds/minutes
+    /**
+     * Constructor.
+     *
+     * @param $pattern string the pattern
+     * @param $noOffsetText string the text to use for UTC, not null
+     */
+    public function __construct($pattern, $noOffsetText)
+    {
+        //Objects->requireNonNull(pattern, "pattern");
+//Objects->requireNonNull(noOffsetText, "noOffsetText");
+        $this->type = $this->checkPattern($pattern);
+        $this->noOffsetText = $noOffsetText;
     }
-    int pos = array[0];
-            if ((type % 2) == 0 && arrayIndex > 1) {
-                if (pos + 1 > parseText->length() || parseText->charAt(pos) != ':') {
-                    return required;
-                }
-                pos++;
-            }
-            if (pos + 2 > parseText->length()) {
-        return required;
+
+    private function checkPattern($pattern)
+    {
+        $i = array_search($pattern, self::$PATTERNS);
+        if ($i === false)
+            throw new RuntimeException("Invalid zone offset pattern: " . $pattern);
+        return $i;
     }
-            char ch1 = parseText->charAt(pos++);
-            char ch2 = parseText->charAt(pos++);
-            if (ch1 < '0' || ch1 > '9' || ch2 < '0' || ch2 > '9') {
-                return required;
-            }
-            int value = (ch1 - 48) * 10 + (ch2 - 48);
-            if (value < 0 || value > 59) {
-                return required;
-            }
-            array[arrayIndex] = value;
-            array[0] = pos;
+
+    public function format(DateTimePrintContext $context, &$buf)
+    {
+        $offsetSecs = $context->getValueField(ChronoField::OFFSET_SECONDS());
+        if ($offsetSecs === null) {
             return false;
         }
 
-        @Override
-        public String toString(){
-String converted = noOffsetText->replace("'", "''");
-            return "Offset(" + PATTERNS[type] + ",'" + converted + "')";
+        $totalSecs = $offsetSecs;
+        if ($totalSecs === 0) {
+            $buf .= $this->noOffsetText;
+        } else {
+            $absHours = Math::abs(($totalSecs / 3600) % 100);  // anything larger than 99 silently dropped
+            $absMinutes = Math::abs(($totalSecs / 60) % 60);
+            $absSeconds = Math::abs($totalSecs % 60);
+            $bufPos = strlen($buf);
+            $output = $absHours;
+            $buf .= ($totalSecs < 0 ? "-" : "+")
+                . Math::div($absHours, 10) . ($absHours % 10);
+            if ($this->type >= 3 || ($this->type >= 1 && $absMinutes > 0)) {
+                $buf .= (($this->type % 2) === 0 ? ":" : "")
+                    . Math::div($absMinutes, 10) . ($absMinutes % 10);
+                $output .= $absMinutes;
+                if ($this->type >= 7 || ($this->type >= 5 && $absSeconds > 0)) {
+                    $buf .= (($this->type % 2) === 0 ? ":" : "")
+                        . Math::div($absSeconds, 10) . ($absSeconds % 10);
+                    $output .= $absSeconds;
+                }
+            }
+            if ($output === 0) {
+                $buf = substr($buf, 0, $bufPos);
+                $buf .= $this->noOffsetText;
+            }
         }
+        return true;
     }
+
+    public function parse(DateTimeParseContext $context, $text, $position)
+    {
+        $length = strlen($text);
+        $noOffsetLen = strlen($this->noOffsetText);
+        if ($noOffsetLen == 0) {
+            if ($position === $length) {
+                return $context->setParsedField(ChronoField::OFFSET_SECONDS(), 0, $position, $position);
+            }
+        } else {
+            if ($position === $length) {
+                return ~$position;
+            }
+
+            if ($context->subSequenceEquals($text, $position, $this->noOffsetText, 0, $noOffsetLen)) {
+                return $context->setParsedField(ChronoField::OFFSET_SECONDS(), 0, $position, $position + $noOffsetLen);
+            }
+        }
+
+// parse normal plus/minus offset
+        $sign = $text[$position];  // IOOBE if invalid position
+        if ($sign === '+' || $sign === '-') {
+            // starts
+            $negative = ($sign == '-' ? -1 : 1);
+            $array = [];
+            $array[0] = $position + 1;
+            if (($this->parseNumber($array, 1, $text, true) ||
+                    $this->parseNumber($array, 2, $text, $this->type >= 3) ||
+                    $this->parseNumber($array, 3, $text, false)) == false
+            ) {
+                // success
+                $offsetSecs = $negative * ($array[1] * 3600 + $array[2] * 60 + $array[3]);
+                return $context->setParsedField(ChronoField::OFFSET_SECONDS(), $offsetSecs, $position, $array[0]);
+            }
+        }
+        // handle special case of empty no offset text
+        if ($noOffsetLen === 0) {
+            return $context->setParsedField(ChronoField::OFFSET_SECONDS(), 0, $position, $position + $noOffsetLen);
+        }
+        return ~$position;
+    }
+
+    /**
+     * Parse a two digit zero-prefixed number.
+     *
+     * @param $array array the array of parsed data, 0=pos,1=hours,2=mins,3=secs, not null
+     * @param $arrayIndex int the index to parse the value into
+     * @param $parseText string the offset ID, not null
+     * @param $required bool whether this number is required
+     * @return bool true if an error occurred
+     */
+    private function parseNumber(array $array, $arrayIndex, $parseText, $required)
+    {
+        if (($this->type + 3) / 2 < $arrayIndex) {
+            return false;  // ignore seconds/minutes
+        }
+        $pos = $array[0];
+        if (($this->type % 2) === 0 && $arrayIndex > 1) {
+            if ($pos + 1 > strlen($parseText) || $parseText[$pos] !== ':') {
+                return $required;
+            }
+            $pos++;
+        }
+        if ($pos + 2 > strlen($parseText)) {
+            return $required;
+        }
+        $ch1 = $parseText[$pos++];
+        $ch2 = $parseText[$pos++];
+        if ($ch1 < '0' || $ch1 > '9' || $ch2 < '0' || $ch2 > '9') {
+            return $required;
+        }
+        $value = ($ch1 - 48) * 10 + ($ch2 - 48);
+        if ($value < 0 || $value > 59) {
+            return $required;
+        }
+        $array[$arrayIndex] = $value;
+        $array[0] = $pos;
+        return false;
+    }
+
+    public function __toString()
+    {
+        $converted = str_replace("'", "''", $this->noOffsetText);
+        return "Offset(" . self::$PATTERNS[$this->type] . ",'" . $converted . "')";
+    }
+}
