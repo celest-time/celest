02bc8029a23aa0cb85705183e106b7f70c6cdf74
Steffen Hanikel <steffen.hanikel@gmail.com>
2016-02-07 18:55:43 +0100

    Added intl extension as dependency plus tests

--- test/Celest/MiscTest.php
+++ test/Celest/MiscTest.php
@@ -5,7 +5,9 @@ namespace Celest;
 
 use Celest\Helper\Long;
 use Celest\Helper\Math;
+use IntlCalendar;
 use PHPUnit_Framework_TestCase;
+use ResourceBundle;
 
 class MiscTest extends PHPUnit_Framework_TestCase
 {
@@ -36,5 +38,31 @@ class MiscTest extends PHPUnit_Framework_TestCase
         $seconds = LocalDateTime::MIN()->toEpochSecond($offset);
         $new = LocalDateTime::ofEpochSecond($seconds, 0, $offset);
         $this->assertEquals(LocalDateTime::MIN(), $new);
+    }
+
+    public function testIntlBundleCase() {
+        $bundle = new ResourceBundle('pt_BR', null);
+        $this->assertEquals('ter', $bundle['calendar']['gregorian']['dayNames']['format']['abbreviated'][2]);
+    }
+
+    public function testIntlBundles()
+    {
+        $bundle = new ResourceBundle('de', 'ICUDATA-region');
+        $this->assertEquals('Deutschland', $bundle['Countries']['DE']);
+
+        $bundle = new ResourceBundle('de', 'ICUDATA-zone');
+        $this->assertEquals('Brüssel', $bundle['zoneStrings']['Europe:Brussels']['ec']);
+    }
+
+    public function testIntlDate() {
+        $date = new \DateTime('2000-02-03 04:05', new \DateTimeZone('UTC'));
+        $formatter1 = \IntlDateFormatter::create('en@calendar=islamic-umalqura', \IntlDateFormatter::FULL, \IntlDateFormatter::FULL, "Europe/Berlin", \IntlDateFormatter::TRADITIONAL);
+        $formatter2 = \IntlDateFormatter::create('@calendar=islamic-civil', \IntlDateFormatter::FULL, \IntlDateFormatter::FULL, "Europe/Berlin", \IntlDateFormatter::TRADITIONAL);
+        // TODO investigate failure
+        //$this->assertEquals('AH 1420 Shawwal 27, Thu 05:05:00 GMT+01:00', $formatter1->format($date), 'islamic-umalqura');
+        $this->assertEquals('AH 1420 Shawwal 27, Thu 05:05:00 GMT+01:00', $formatter2->format($date), 'islamic-civil');
+
+        $formatter = \IntlDateFormatter::create('@calendar=japanese', \IntlDateFormatter::FULL, \IntlDateFormatter::FULL, "Europe/Berlin", \IntlDateFormatter::TRADITIONAL);
+        $this->assertEquals('Heisei 12 M02 3, Thu 05:05:00 GMT+01:00', $formatter->format($date));
     }
 }
