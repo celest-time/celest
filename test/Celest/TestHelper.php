ed8a6736fced6788df9834e7bcaf35045a6a2551
Steffen Hanikel <steffen.hanikel@gmail.com>
2016-02-09 23:23:45 +0100

    Fixed lenient parsing WIP

--- test/Celest/TestHelper.php
+++ test/Celest/TestHelper.php
@@ -28,8 +28,8 @@ class TestHelper
     }
 
     public static function getRussianJanFormat() {
-        if (version_compare(INTL_ICU_DATA_VERSION, "52", "<")) {
-            return 'янв.';
+        if (version_compare(INTL_ICU_DATA_VERSION, "49", "<")) {
+            return 'янв';
         } else if (version_compare(INTL_ICU_DATA_VERSION, "54", "<")) {
             return 'Янв.';
         } else {
