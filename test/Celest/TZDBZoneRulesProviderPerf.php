bcedfa49a8aa36571a6f3aa55c4d6de881cea8d3
Steffen Hanikel <steffen.hanikel@meinfernbus.de>
2016-03-13 22:15:35 +0100

    Added simple perf test for TZDB rules loading

---
+++ test/Celest/TZDBZoneRulesProviderPerf.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Celest;
+
+use Celest\Temporal\ChronoUnit;
+use Celest\Zone\TZDBZoneRulesProvider;
+use Celest\Zone\ZoneRulesProvider;
+
+class TZDBZoneRulesProviderPerf
+{
+    public static function main($argv)
+    {
+        require __DIR__ . '/../../vendor/autoload.php';
+        array_splice($argv, 0, 1);
+        (new TZDBZoneRulesProviderPerf())->run($argv);
+    }
+
+    private function run($argv)
+    {
+        $ids = ZoneRulesProvider::getAvailableZoneIds();
+
+        $start = Instant::now();
+        foreach($ids as $id) {
+            TZDBZoneRulesProvider::getRules($id, false);
+        }
+        $end = Instant::now();
+
+        echo 'First run: ', ChronoUnit::MILLIS()->between($start, $end), "ms, count: ", count($ids), "\n";
+    }
+
+}
+
+global $argv;
+TZDBZoneRulesProviderPerf::main($argv);
